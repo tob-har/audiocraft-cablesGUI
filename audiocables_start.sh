771100b98a5aabb85d01ccb0f3415a849ce179c1
Tobias Hartmann <login@hartmanntobias.de>
2023-10-04 08:23:44 +0200

    edits

--- audiocables_start.sh
+++ audiocables_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Navigate to the first directory and run the script
-cd /Users/tobias.hartmann/Documents/audiocraft-main-cablesGUI
+cd /Users/tobias.hartmann/Documents/audiocraft-cablesGUI
 python serverCables.py &
 
 # Wait a few seconds to ensure the above script starts properly
 sleep 12
 
 # Navigate to the second directory and run the http.server
-cd /Users/tobias.hartmann/Documents/audiocraft-main-cablesGUI/cables_audiocraftAPI4
+cd /Users/tobias.hartmann/Documents/audiocraft-cablesGUI/cables_audiocraftAPI4
 python -m http.server & 
